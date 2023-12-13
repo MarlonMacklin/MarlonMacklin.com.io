e2166584edc23043f47b04b3c61cf5f215da8265
Marlon Macklin <126262678+MarlonMacklin@users.noreply.github.com>
2023-12-13 14:12:17 -0500

    Add files via upload

---
+++ ch10/apply/scripts/scripts.js
@@ -0,0 +1,10 @@
+/*Marlon Macklin, 12/12/2023*/
+
+/*Function to display content*/
+script.js
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f"
+    text.style.fontSize = "2em";
+}
