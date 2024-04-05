22fb08347a4f62be2e4b89a129d533277ecfe1b7
Marlon Macklin <126262678+MarlonMacklin@users.noreply.github.com>
2024-04-05 14:03:23 -0400

    Add files via upload

---
+++ JavaScript ch1/JavaScript 1-4/project02/project01-02.js
@@ -0,0 +1,15 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Marlon Macklin
+      Date:  4/5/2024   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+service1Name = "Basic", service2Name = "Express", service3Name = "Extreme",
+service4Name = "Ultimate", service1Speed = "0 Mbps", service2speed = "100 Mbps",
+service3speed = "500 Mpbs", service4Speed = "1 Gig"
---
+++ JavaScript ch1/JavaScript 1-4/project02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Marlon Macklin
+      Date:  4/5/2024   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
