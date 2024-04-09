a4a657355f72242ffc6949214f22fc5265c24508
Marlon Macklin <126262678+MarlonMacklin@users.noreply.github.com>
2024-04-09 12:38:36 -0400

    Add files via upload

---
+++ JavaScript 1-4/project04/project01-04.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Marlon Macklin
+      Date: 4/5/2024    
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+homeStreet = "1MainSt.", homeCity = "Sicilia", homeState = "MA", homeCode = "02103",
+workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104"
