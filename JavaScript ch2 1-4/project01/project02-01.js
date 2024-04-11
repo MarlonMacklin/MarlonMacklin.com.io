5237b123732bba52f7643885ad805619933c6f00
Marlon Macklin <126262678+MarlonMacklin@users.noreply.github.com>
2024-04-11 12:48:39 -0400

    Add files via upload

---
+++ JavaScript ch2 1-4/project01/project02-01.js
@@ -0,0 +1,30 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Marlon Macklin
+      Date:   4/10/2024
+
+      Filename: project02-01.js
+ */
+      function CelsiusToFahrenheit() {
+        Cv=document.getElementById("cValue");
+        F = ((fV.value - 32) / 1.8);
+        Cv.value=F;
+        //return F;   
+    }
+    
+    fV=document.getElementById("fValue");
+    fV.addEventListener("change", CelsiusToFahrenheit);
+     
+     //Function Convert Fahrenheit to Celsius
+    function FahrenheitToCelsius() {
+        Fv=document.getElementById("fValue");
+        C = (cV.value * 1.8 + 32);
+        Fv.value=C;
+        //return C;   
+    }
+    
+    cV=document.getElementById("cValue");
+    cV.addEventListener("change", FahrenheitToCelsius);
