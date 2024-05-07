21e20b22fff1741ec6bd13d361994a67aae85d3d
Marlon Macklin <126262678+MarlonMacklin@users.noreply.github.com>
2024-05-07 12:43:38 -0400

    Add files via upload

--- JavaScript ch5 1-4/project04/footnotes.js
+++ JavaScript ch5 1-4/project04/footnotes.js
@@ -4,8 +4,8 @@
       Project 05-04
 
       Project to display footnotes in a popup window
-      Author: 
-      Date:   
+      Author: Marlon Macklin
+      Date: 5/7/2024  
 
       Filename: footnotes.js
 */
